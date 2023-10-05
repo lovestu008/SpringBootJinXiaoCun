d1c062d15d49eb6483c6bb57cc573f4b97f91935
hwj <1325277624@qq.com>
2023-10-05 14:58:11 +0800

    generator User测试

--- src/main/java/com/xxxx/supermarket/entity/User.java
+++ src/main/java/com/xxxx/supermarket/entity/User.java
@@ -1,4 +1,73 @@
 package com.xxxx.supermarket.entity;
 
 public class User {
+    private Integer id;
+
+    private String bz;
+
+    private String password;
+
+    private String trueName;
+
+    private String userName;
+
+    private String remarks;
+
+    private Integer isDel;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBz() {
+        return bz;
+    }
+
+    public void setBz(String bz) {
+        this.bz = bz == null ? null : bz.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getTrueName() {
+        return trueName;
+    }
+
+    public void setTrueName(String trueName) {
+        this.trueName = trueName == null ? null : trueName.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks == null ? null : remarks.trim();
+    }
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
 }
