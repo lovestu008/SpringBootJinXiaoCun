96c98b0dae495cd7ca9ab60cd22cd05aacc03ee4
hwj <1325277624@qq.com>
2023-10-10 17:16:18 +0800

    忽略操作

--- src/main/java/com/xxxx/supermarket/dao/ProviderMapper.java
+++ src/main/java/com/xxxx/supermarket/dao/ProviderMapper.java
@@ -3,7 +3,6 @@ package com.xxxx.supermarket.dao;
 import com.xxxx.supermarket.base.BaseMapper;
 import com.xxxx.supermarket.entity.Customer;
 import com.xxxx.supermarket.entity.Provider;
-import io.swagger.models.auth.In;
 
 import java.util.List;
 
