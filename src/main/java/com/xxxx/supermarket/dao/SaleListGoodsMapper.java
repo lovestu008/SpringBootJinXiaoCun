c816c956a8774700de81405b4777cc6ad140b83d
Hiyume <1121438454@qq.com>
2023-10-10 20:37:01 +0800

    小更新

--- src/main/java/com/xxxx/supermarket/dao/SaleListGoodsMapper.java
+++ src/main/java/com/xxxx/supermarket/dao/SaleListGoodsMapper.java
@@ -2,10 +2,12 @@ package com.xxxx.supermarket.dao;
 
 import com.xxxx.supermarket.base.BaseMapper;
 import com.xxxx.supermarket.entity.SaleListGoods;
+import com.xxxx.supermarket.model.SaleListGoodsModel;
 
 import java.util.List;
 
 public interface SaleListGoodsMapper extends BaseMapper<SaleListGoods,Integer> {
+
 
 
     /**
@@ -14,4 +16,8 @@ public interface SaleListGoodsMapper extends BaseMapper<SaleListGoods,Integer> {
      * @return
      */
     List<SaleListGoods> querySaleListGoodsBySaleListId(Integer id);
+
+    //查询商品销量前五
+    List<SaleListGoodsModel> selectSaleListGoods();
+
 }
